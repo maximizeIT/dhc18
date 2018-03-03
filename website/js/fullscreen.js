97785cd7ef54d878e97f5a87a25a630f77b930d1
Max Scholz <max.scholz@maximize-it.eu>
2018-03-03 15:58:22 +0100

    added draft website

---
+++ website/js/fullscreen.js
@@ -0,0 +1,22 @@
+(function () {
+    var viewFullScreen = document.getElementById("view-fullscreen");
+    
+    if (viewFullScreen) {
+        viewFullScreen.addEventListener("click", function () {
+            var docElm = document.documentElement;
+            if (docElm.requestFullscreen) {
+                docElm.requestFullscreen();
+            }
+            else if (docElm.msRequestFullscreen) {
+                docElm = document.body; //overwrite the element (for IE)
+                docElm.msRequestFullscreen();
+            }
+            else if (docElm.mozRequestFullScreen) {
+                docElm.mozRequestFullScreen();
+            }
+            else if (docElm.webkitRequestFullScreen) {
+                docElm.webkitRequestFullScreen();
+            }
+        }, false);
+    }
+})();
