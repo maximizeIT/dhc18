a37ae62447efdfabbd7cc801bf477d22363ef07a
Max Scholz <max.scholz@maximize-it.eu>
2018-03-04 01:29:53 +0100

    start page ready. Mgmt page next

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -51,21 +51,19 @@
             if (pageType === "s-page") {
                 fadeDashBoard();
                 $('.' + page).addClass('slidePageInFromLeft').removeClass('slidePageBackLeft');
-            } else {
-                $('.' + page).addClass('openpage');
-                fadeDashBoard();
             }
         });
     });
 
-    $('#maplink').each(function() {
+    $('#btnLogin').each(function() {
         var $this = $(this);
 
         $this.on('click', function() {
-            $('.search-page').removeClass('slidePageInFromLeft').addClass('slidePageBackLeft');
+            $('.login-page').removeClass('slidePageInFromLeft').addClass('slidePageBackLeft');
+            $('.mgmt-page').removeClass('slidePageBackRight')
             // fadeDashBoard();
-            $('.map-page').addClass('slidePageInFromLeft').removeClass('slidePageBackLeft');
-            $('.map-page').css({
+            $('.mgmt-page').addClass('slidePageInFromRight').removeClass('slidePageBackLeft');
+            $('.mgmt-page').css({
                     'background-color': 'white',
                     'color': 'black'
                 })
@@ -83,47 +81,9 @@
         showDashBoard();
     });
 
+    $('.r-logout-button').click(function() {
+        $(this).parent().removeClass('slidePageInFromRight').addClass('slidePageBackRight');
+        showDashBoard();
+    });
+
 })();
-
-// var myCSV = "Species,Identifiant\r\n";
-// myCSV += "Species A,320439\r\n";
-// myCSV += "Species B,349450\r\n";
-// myCSV += "Species C,43435904\r\n";
-// myCSV += "Species D,320440\r\n";
-// myCSV += "Species E,349451\r\n";
-// myCSV += "Species F,43435905\r\n";
-
-// $(function() {
-//   function processData(allText) {
-//     var record_num = 2; // or however many elements there are in each row
-//     var allTextLines = allText.split(/\r\n|\n/);
-//     var lines = [];
-//     var headings = allTextLines.shift().split(',');
-//     while (allTextLines.length > 0) {
-//       var tobj = {}, entry;
-//       entry = allTextLines.shift().split(',');
-//       tobj['label'] = entry[0];
-//       tobj['value'] = entry[1];
-//       lines.push(tobj);
-//     }
-//     return lines;
-//   }
-
-//   // Storage for lists of CSV Data
-//   var lists = [];
-//   // Get the CSV Content
-//   // $.get("reference.csv", function(data) {
-//   //   lists = processData(data);
-//   // });
-//     lists = processData(myCSV);
-
-//   $("#input-16").autocomplete({
-//     minLength: 3,
-//     source: lists,
-//     select: function(event, ui) {
-//       $("#input-16").val(ui.item.label);
-//       $("#identifiant").val(ui.item.value);
-//       return false;
-//     }
-//   });
-// });
